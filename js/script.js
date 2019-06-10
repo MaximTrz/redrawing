8219a9c831799b3f46cf678c3ffc82db7fa6f9e5
Maxim <maxim.trz@gmail.com>
2019-06-10 11:15:18 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -1,46 +1,87 @@
 window.addEventListener('DOMContentLoaded', ()=>{
 
-  const divider = document.querySelector(".divider"),
-        leftBlock = document.querySelector(".imageBlock_left"),
-        rightBlock = document.querySelector(".imageBlock_right"),
-        wrapper = document.querySelector(".imageBox"),
-        imgLeft = document.querySelector(".imageBlock_left>img");
-        imgRight = document.querySelector(".imageBlock_right>img");
-        
-    leftBlock.style.width = wrapper.offsetWidth + divider.offsetWidth +'px';
-    leftBlock.style.height = wrapper.offsetHeight + 'px';
-    imgLeft.style.width = wrapper.offsetWidth + divider.offsetWidth +'px';
 
-    rightBlock.style.width = wrapper.offsetWidth + divider.offsetWidth + 'px';
-    rightBlock.style.height = wrapper.offsetHeight + 'px';
-    imgRight.style.width = wrapper.offsetWidth + divider.offsetWidth + 'px';
+redrawingImg = new redrawing({
+  divider: '.divider',
+  leftBlock: '.imageBlock_left',
+  rightBlock: '.imageBlock_right',
+  wrapper: '.imageBox',
+  imgLeft: '.imageBlock_left>img',
+  imgRight: '.imageBlock_right>img'
+});
 
-    divider.style.left = "50%";
-    leftBlock.style.width = (wrapper.offsetWidth/2) + 'px';
+function redrawing(settings){
+    
+  this.divider = document.querySelector(settings.divider);
+  this.leftBlock = document.querySelector(settings.leftBlock);
+  this.rightBlock = document.querySelector(settings.rightBlock);
+  this.wrapper = document.querySelector(settings.wrapper);
+  this.imgLeft = document.querySelector(settings.imgLeft);
+  this.imgRight = document.querySelector(settings.imgRight);
 
+  
+  this.leftBlock.style.width = this.wrapper.offsetWidth + this.divider.offsetWidth +'px';
+  this.leftBlock.style.height = this.wrapper.offsetHeight + 'px';
+  this.imgLeft.style.width = this.wrapper.offsetWidth + this.divider.offsetWidth +'px';
+
+  this.rightBlock.style.width = this.wrapper.offsetWidth + this.divider.offsetWidth + 'px';
+  this.rightBlock.style.height = this.wrapper.offsetHeight + 'px';
+  this.imgRight.style.width = this.wrapper.offsetWidth + this.divider.offsetWidth + 'px';
+
+  this.divider.style.left = "50%";
+  this.leftBlock.style.width = (this.wrapper.offsetWidth/2) + 'px';
+
+
+   this.moveAt = function moveAt(e) {
+    
+    let posX;
+
+    if(e.which=="1"){
+      posX = (e.pageX-this.wrapper.getBoundingClientRect().x) - this.divider.offsetWidth / 2;      
+    }
 
     
+    if(e.which=="0"){
+      posX = (e.targetTouches[0].clientX-this.wrapper.getBoundingClientRect().x) - this.divider.offsetWidth / 2;      
+    };
+    
+    if ((posX>=0)&&(posX<=this.wrapper.offsetWidth)){
+      this.divider.style.left = posX  + 'px';
+      this.leftBlock.style.width = posX + 'px';
+      this.leftBlock.style.width = posX + 'px';  
+    }
+    
+  }
 
-  divider.addEventListener('mousedown', (e)=>{  
 
-    function moveAt(e) {
-      let posX = e.pageX - divider.offsetWidth / 2;
-      if ((posX>=0)&&(posX<=wrapper.offsetWidth)){
-        divider.style.left = posX  + 'px';
-        leftBlock.style.width = posX + 'px';        
+
+  this.divider.addEventListener('mousedown', (e)=>{
+      this.wrapper.onmousemove = (e)=> {
+        this.moveAt(e);        
       }      
-    }
-
-    wrapper.onmousemove = function(e) {
-      moveAt(e);
-    }
-
   });
 
-  divider.addEventListener("mouseup", ()=>{
-    wrapper.onmousemove = null;
-    divider.onmouseup = null;
+
+
+  this.divider.addEventListener("touchstart", (e)=>{
+    this.moveAt(e);    
   });
+
+  this.divider.addEventListener("mouseup", ()=>{
+    
+    this.wrapper.onmousemove = null;
+    this.divider.onmouseup = null;
+    
+  });
+    
+  this.wrapper.onmousedown = function(){    
+    return false;
+  };
+
+} 
+
+
+
 
 });
-    
+
